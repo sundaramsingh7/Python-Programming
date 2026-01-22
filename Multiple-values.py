4594d07a5911de3dd60ef1409367fb58aa08b9a5
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-22 23:07:09 +0530

    unpack a collection

--- Multiple-values.py
+++ Multiple-values.py
@@ -9,3 +9,19 @@ print(x)
 print(y)
 print(z)
 
+# Unpack a collection
+# 1.(list)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+# 2.(tuple)
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
