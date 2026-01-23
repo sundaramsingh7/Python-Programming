c778b485482deb1b9202b4af26eb75e203f2efcc
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-23 23:18:11 +0530

    complete tuple, using length fuction in tuple and datatype of tuple

---
+++ Tuple.py
@@ -0,0 +1,22 @@
+#Tuples are used to store multiple items in a single variable.
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#To determine how many items a tuple has, use the len() function:
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#Tuple items can be of any data type:
+#String, int and boolean data types:
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#The tuple() Constructor
+#It is also possible to use the tuple() constructor to make a tuple.
+#Example - Using the tuple() method to make a tuple:
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+
