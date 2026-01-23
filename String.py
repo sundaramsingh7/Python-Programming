d635bcdfae2e55190edb05e44daf18fced098cb0
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-23 23:36:17 +0530

    complete string, assign to a variable and slicing process

---
+++ String.py
@@ -0,0 +1,29 @@
+# String : Strings in python are surrounded by either single quotation marks, or double quotation marks.
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Assign string to a variable
+a = "Hello"
+print(a)
+
+#Slicing
+#You can return a range of characters by using the slice syntax.
+#Specify the start index and the end index, separated by a colon, to return a part of the string.
+b = "Hello, World!"
+print(b[2:5])
+
+#Slice From the Start
+#By leaving out the start index, the range will start at the first character:
+b = "Hello, World!"
+print(b[:5])
+
+
+#Slice To the End
+#By leaving out the end index, the range will go to the end:
+b = "Hello, World!"
+print(b[2:])
+
+
+
+
