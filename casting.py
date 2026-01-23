aaf29f25c60c3c3c64f10d4436400098c5c1111a
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-23 23:27:04 +0530

    casting in python by the constructor function such as Int, Float, String

---
+++ casting.py
@@ -0,0 +1,17 @@
+# casting in python has been done by the constructor function
+# such as int, float, str
+# 1. Integer
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+# 2.Float
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+# 3.String
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
