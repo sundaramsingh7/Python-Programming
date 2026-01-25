f06028fe687f77fbc4f7b2f0dd9530a29ab7b5f9
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-25 22:56:39 +0530

    Operators in python

---
+++ Operators.py
@@ -0,0 +1,11 @@
+#Python Operators
+#Operators are used to perform operations on variables and values.
+#In the example below, we use the + operator to add together two values:
+
+#Example 1
+print(10 + 5)
+
+#Example 2
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
