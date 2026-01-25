4a396148664da2539dc54e5171cbbb53b6c49623
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-25 22:53:03 +0530

    boolean types and its example

--- Boolean.py
+++ Boolean.py
@@ -12,3 +12,21 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+
+  #Evaluate Values and Variables
+  #The bool() function allows you to evaluate any value, and give you True or False in return,
+
+# Example 1
+#Evaluate a string and a number:
+print(bool("Hello"))
+print(bool(15))
+
+#Example 2
+#Evaluate two variables:
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
