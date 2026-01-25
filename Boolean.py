a4479f6266a2670a57a3f8e3d68524e3446de9cb
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-25 22:48:02 +0530

    Boolean in python

---
+++ Boolean.py
@@ -0,0 +1,14 @@
+#Boolean: Booleans represent one of two values: True or False.
+#Example 1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9
+
+#Example 2
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
