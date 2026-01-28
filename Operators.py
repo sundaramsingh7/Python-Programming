8ed4832a5f53b0d92462cb44e112044d3e3b2b50
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-28 17:02:08 +0530

    complete Arithmetic and Assignment operator

--- Operators.py
+++ Operators.py
@@ -9,3 +9,30 @@ print(10 + 5)
 sum1 = 100 + 50      # 150 (100 + 50)
 sum2 = sum1 + 250    # 400 (150 + 250)
 sum3 = sum2 + sum2   # 800 (400 + 400)
+
+# 1. Arithmetic operator
+#Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# 2.Assignment Operators
+#Assignment operators are used to assign values to variables:
+
+count = 5
+count += 2  # Equivalent to count = count + 2
+print(count)
+# Output: 7
+
+message = "Hello"
+message += ", World!" # Concatenation also works
+print(message)
+# Output: Hello, World!
