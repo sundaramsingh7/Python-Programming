46bb67bb986e4d70a84aba419289d9d3d940e67b
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-30 21:03:13 +0530

    Bitwise operator and operator procedence

--- Operators.py
+++ Operators.py
@@ -114,4 +114,8 @@ fruits = ["apple", "banana", "cherry"]
 print("pineapple" not in fruits)
 
 # 7.Bitwise Operators
-#Bitwise operators are used to compare (binary) numbers:
+#Bitwise operators are used to compare (binary) numbers: &, |, ^, ~, >>, <<
+
+print(6 & 3) #The & operator compares each bit and set it to 1 if both are 1, otherwise it is set to 0:
+print(6 | 3) #The | operator compares each bit and set it to 1 if one or both is 1, otherwise it is set to 0:
+print(6 ^ 3) #The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
