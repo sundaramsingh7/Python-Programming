58149b210bc863d9cd9d1fb058f1c56f9fb84e29
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-30 21:16:08 +0530

    Logical condition and if statement

---
+++ if-else.py
@@ -0,0 +1,25 @@
+#Python Conditions and If statements
+#Equals: a == b
+#Not Equals: a != b
+#Less than: a < b
+#Less than or equal to: a <= b
+#Greater than: a > b
+#Greater than or equal to: a >= b
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+  #Checking if a number is positive:
+  number = 15
+if number > 0:
+  print("The number is positive")
+
+  #Multiple statements in an if block:
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
