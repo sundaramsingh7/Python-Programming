f0576b431f2704d5b50c3e543652221dc3fe5e28
sundaramsingh7 <sundaram31singh@gmail.com>
2026-01-31 22:50:14 +0530

    complete Elif condition

--- if-else.py
+++ if-else.py
@@ -23,3 +23,41 @@ if age >= 18:
   print("You are an adult")
   print("You can vote")
   print("You have full legal rights")
+
+  #Python Elif Statement :The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+  a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#Multiple Elif Statements:
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+# exapmle - 2
+#Day of the week checker:
+
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
