f48dd5f84496d17b3f8e45013472e363dc02f2d3
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-04 23:20:16 +0530

    some more exapmle of Dates in python

--- DateTime.py
+++ DateTime.py
@@ -13,3 +13,24 @@ x = datetime.datetime.now()
 
 print(x.year)
 print(x.strftime("%A"))
+
+# Creating date time object : The datetime() class requires three parameters to create a date: year, month, day.
+#Example : Create a date object:
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
+#The strftime() Method
+#The datetime object has a method for formatting date objects into readable strings.
+#The method is called strftime(), and takes one parameter, format, to specify the format of the returned string:
+
+#Example : Display the name of the month:
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
+
