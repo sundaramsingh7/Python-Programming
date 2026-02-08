b22182f6ca2a174bac10873cb33e3c6e443d9e70
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-08 23:38:27 +0530

    complete int , float  with example in number section

--- Number.py
+++ Number.py
@@ -4,3 +4,22 @@ z = 1j   # complex
 print(type(x))
 print(type(y))
 print(type(z))
+
+# Int : Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Float : Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
