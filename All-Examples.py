ba71b4eb4d873648b856d47f1d23ffca0427183b
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-12 23:23:29 +0530

    adding two intrgers with example

---
+++ All-Examples.py
@@ -0,0 +1,22 @@
+# Add two integer
+a = 15
+b = 12
+
+# Adding two numbers
+res = a + b
+print(res)
+
+
+# using function :
+# function to add two numbers
+def add(a, b):
+    return a + b
+
+# initializing numbers
+a = 10
+b = 5
+
+# calling function
+res = add(a,b)
+
+print(res)
