76e34e54bac3c307380aabd0979748f7b3616276
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-13 23:21:57 +0530

    using lambda, operator and sum function

--- All-Examples.py
+++ All-Examples.py
@@ -20,3 +20,20 @@ b = 5
 res = add(a,b)
 
 print(res)
+
+
+#Using Lambda Function : A lambda function is an anonymous,
+#  one-line function that can perform operations without defining a full function block. It is useful for short, simple calculations.
+res = lambda a, b: a + b
+print(res(10, 5))
+
+#Using operator.add : operator module provides a functionally equivalent method to the + operator.
+#  This is useful when working with functional programming or reducing redundancy in complex operations.
+import operator
+print(operator.add(10, 5))
+
+#Using sum() : sum() function is commonly used to add multiple numbers in an iterable like lists or tuples. It provides a simple way to add elements without using loops or explicit operators.
+
+print(sum([10, 5]))
+
+
