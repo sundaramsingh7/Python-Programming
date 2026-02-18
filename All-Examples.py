d525b6fbd5b0d4967cfbe636b8e75d8540a739fb
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-18 23:18:07 +0530

    complete python Dictionary

--- All-Examples.py
+++ All-Examples.py
@@ -106,6 +106,80 @@ def fact(m):
 print(fact(7))
 
 
+# Python Dictionary :A Python dictionary is a data structure that stores data in key-value pairs, where each key is unique and is used to retrieve its associated value. It is mainly used when you want to store and access data by a name (key) instead of by position like in a list.
 
+#Example: This example shows how a dictionary stores data using keys and values
+data = { "name": "Jake", "age": 22 }
+print(data)
 
+#Creating a Dictionary :A dictionary is created by writing key-value pairs inside { }, where each key is connected to a value using colon (:). A dictionary can also be created using the dict() function.
  
+d1 = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(d1)
+
+# using dict() constructor
+d2 = dict(a = "Geeks", b = "for", c = "Geeks")
+print(d2)
+
+#Adding and Updating Dictionary Items
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d[1] = "Python dict"
+print(d)
+
+#Removing Dictionary Items :
+#del: removes an item using its key
+#pop(): removes the item with the given key and returns its value
+#clear(): removes all items from the dictionary
+#popitem(): removes and returns the last inserted key–value pair
+
+
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks', 'age':22}
+
+# Using del 
+del d["age"]
+print(d)
+
+# Using pop() 
+val = d.pop(1)
+print(val)
+
+# Using popitem()
+key, val = d.popitem()
+print(f"Key: {key}, Value: {val}")
+
+# Using clear()
+d.clear()
+print(d)
+
+
+#Iterating Through a Dictionary
+d = {1: 'Geeks', 2: 'For', 'age':22}
+
+# Iterate over keys
+for key in d:
+    print(key)
+
+# Iterate over values
+for value in d.values():
+    print(value)
+
+# Iterate over key-value pairs
+for key, value in d.items():
+    print(f"{key}: {value}")
