09ac259d46e4b72ab718807493f3ee7db21fd9d3
sundaramsingh7 <sundaram31singh@gmail.com>
2026-02-24 22:41:41 +0530

    creating tuples and accessing tuples

--- All-Examples.py
+++ All-Examples.py
@@ -401,3 +401,66 @@ print(set1)
 d = {1: "One", 2: "Two", 3: "Three"}
 set1 = set(d)
 print(set1)
+
+
+# Python Tuples ;A tuple in Python is an immutable ordered collection of elements.
+
+# Creating a Tuple
+tup = ()
+print(tup)
+
+# Using String
+tup = ('Geeks', 'For')
+print(tup)
+
+# Using List
+li = [1, 2, 4, 5, 6]
+print(tuple(li))
+
+# Using Built-in Function
+tup = tuple('Geeks')
+print(tup)
+
+
+# Creating a Tuple with Mixed Datatypes.
+tup = (5, 'Welcome', 7, 'Geeks')
+print(tup)
+
+# Creating a Tuple with nested tuples
+tup1 = (0, 1, 2, 3)
+tup2 = ('python', 'geek')
+tup3 = (tup1, tup2)
+print(tup3)
+
+# Creating a Tuple with repetition
+tup1 = ('Geeks',) * 3
+print(tup1)
+
+# Creating a Tuple with the use of loop
+tup = ('Geeks')
+n = 5
+for i in range(int(n)):
+    tup = (tup,)
+    print(tup)
+
+
+#Accessing of Tuples
+# Accessing Tuple with Indexing
+tup = tuple("Geeks")
+print(tup[0])
+
+# Accessing a range of elements using slicing
+print(tup[1:4])  
+print(tup[:3])
+
+# Tuple unpacking
+tup = ("Geeks", "For", "Geeks")
+
+# This line unpack values of Tuple1
+a, b, c = tup
+print(a)
+print(b)
+print(c)
+
+
+# 
