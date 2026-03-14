5c9c97dae5e828ad805134ef81d4508c7f30aa2f
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-14 22:10:12 +0530

    To calculate area of triangle

--- All-Examples.py
+++ All-Examples.py
@@ -513,3 +513,22 @@ substr = text [7:18]
 print("Original text:",text)
 
 print("extracted substring:",substr)
+
+# Python programming to calculate Area of triangle
+
+
+a = 5
+b = 6
+c = 7
+
+# Uncomment below to take inputs from the user
+# a = float(input('Enter first side: '))
+# b = float(input('Enter second side: '))
+# c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
