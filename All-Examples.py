ea81b4d827164fa6c9e6c334c768b3b9f51b0d19
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-19 22:33:56 +0530

    Swapping without temporary variable

--- All-Examples.py
+++ All-Examples.py
@@ -564,3 +564,11 @@ y = temp
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
+
+# Without temporary variable
+x = 5
+y = 10
+
+x, y = y, x
+print("x =", x)
+print("y =", y)
