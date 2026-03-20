a1d897d62728db912b48946627b42755093307f6
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-20 21:58:28 +0530

    Convert kilometer to miles

--- All-Examples.py
+++ All-Examples.py
@@ -572,3 +572,15 @@ y = 10
 x, y = y, x
 print("x =", x)
 print("y =", y)
+
+
+# Convert Kilometers to Miles
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
