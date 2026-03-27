03e454e1175c11ad798b59a50c6c65877807dfbc
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-27 22:20:36 +0530

    To display multiplication table

--- All-Examples.py
+++ All-Examples.py
@@ -691,3 +691,17 @@ num = 7
 # call the factorial function
 result = factorial(num)
 print("The factorial of", num, "is", result)
+
+
+
+
+## Multiplication table (from 1 to 10) in Python
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
