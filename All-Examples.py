12e726bff26f8965a8fc0eaea45e42724c675fd7
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-29 22:53:56 +0530

    To check whether number is positive, negative or zero by using Nested if

--- All-Examples.py
+++ All-Examples.py
@@ -728,10 +728,21 @@ for num in range(lower, upper + 1):
 
 
 # Using if...elif...else
-#num = float(input("Enter a number: "))
+num = float(input("Enter a number: "))
 if num > 0:
    print("Positive number")
 elif num == 0:
    print("Zero")
+else:
+   print("Negative number")
+
+
+# Using Nested if 
+num = float(input("Enter a number: "))
+if num >= 0:
+   if num == 0:
+       print("Zero")
+   else:
+       print("Positive number")
 else:
    print("Negative number")
