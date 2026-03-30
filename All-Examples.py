53f2236a070621c3216168a3b3fc0c49c75142de
sundaramsingh7 <sundaram31singh@gmail.com>
2026-03-30 22:32:49 +0530

    To find armstrong number in an certain interval

--- All-Examples.py
+++ All-Examples.py
@@ -746,3 +746,29 @@ if num >= 0:
        print("Positive number")
 else:
    print("Negative number")
+
+
+
+
+# To find armstrong number in an interval
+# Program to check Armstrong numbers in a certain interval
+
+lower = 100
+upper = 2000
+
+for num in range(lower, upper + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
