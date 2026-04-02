d221ee0579d42f9bbbb0fb8abfcbd04e96d95880
sundaramsingh7 <sundaram31singh@gmail.com>
2026-04-02 23:50:02 +0530

    To find the factors of a numbers

--- All-Examples.py
+++ All-Examples.py
@@ -780,3 +780,19 @@ for num in range(lower, upper + 1):
 
 c = 'p'
 print("The ASCII value of '" + c + "' is", ord(c))
+
+
+
+
+# Python Program to find the factors of a number
+
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
